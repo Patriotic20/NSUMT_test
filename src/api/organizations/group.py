2d49e148085e9d51439af35dd32dd30470176145
Patriotic20 <pterest160@gmail.com>
2025-03-11 13:57:40 +0500

    Add some name

--- src/api/organizations/group.py
+++ src/api/organizations/group.py
@@ -17,25 +17,11 @@ async def add_group(
     group: GroupCreateRequest,
     db: AsyncSession = Depends(get_db)):
     
-    new_group = await main_crud.create(db, obj_in=group)
-    
-    stmt = select(Faculty).where(Faculty.id == new_group.faculty_id)
-    result = await db.execute(stmt)
-    faculty = result.scalars().first()  # Extract the actual faculty object
-
-    if not faculty:
-        raise HTTPException(status_code=404, detail="Faculty not found")
-
-    return {
-        "id": new_group.id,
-        "name": new_group.name,
-        "faculty": faculty.name,
-        "faculty_id": faculty.id
-    }
+    return await main_crud.create(db, obj_in=group)
 
 @group_router.get("/group-get-all")
 async def get_group(db: AsyncSession = Depends(get_db)):
-    return await main_crud.get_all(db)
+    return await main_crud.get_all_name(db)
 
 @group_router.get("/group-get/{group_id}")
 async def get_group_by_id(group_id: int, db: AsyncSession = Depends(get_db)):
